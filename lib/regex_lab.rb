5db8ac47eb9edbd6ad430fcc393e3269d6a94437
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-23 19:04:06 +1000

    commit tree d62866fd2342f30a67d19290f2b29641194b39e3

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,14 +1,10 @@
 def starts_with_a_vowel?(word)
-  !!word[0].match(/[aeiouAEIOU]/)
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
   arr = text.split(" ")
-  result = []
-  arr.each do |word|
-    word.scan(/\A+un/) !=[] && word.scan(/ing\b/)!=[] && result << word
-  end
-  result
+  result = arr.select {|word| word.match(/^un.*ing$/)}
 end
 
 def words_five_letters_long(text)
@@ -16,10 +12,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\A+[A-Z]/) != [] && text[-1] == "." && text.split("").slice(0..-2).none?{|word| word=="."}
+  !!text.match(/^[A-Z][^\.]*[\.\?\!]$/)
 end
 
 def valid_phone_number?(phone)
   phone_no_specials = phone.gsub(/[) (-]/,"")
-  phone_no_specials.length == 10 && phone_no_specials.scan(/[A-Za-z]/) == [] ? true : false
+  !!phone_no_specials.match(/[0-9]{10}/)
 end
